fec74775a2fc8721dd08bbb956491de34b2b7bd7
chspower1 <chspower1@gmail.com>
2022-09-06 02:45:30 +0900

    4.9강 시작전

--- server.js
+++ server.js
@@ -1,22 +1,92 @@
 import { ApolloServer, gql } from "apollo-server";
 
+const tweets = [
+    {
+        id: "1",
+        text: "first!",
+    },
+    { id: "2", text: "Second!" },
+];
+
+const users = [
+    {
+        id: "1",
+        firstName: "Hosung",
+        lastName: "Cho",
+    },
+    {
+        id: "2",
+        firstName: "Hanuel",
+        lastName: "Kim",
+    },
+];
+
+const resolvers = {
+    Query: {
+        allTweets() {
+            return tweets;
+        },
+        tweet(root, { id }) {
+            console.log(root);
+            return tweets.find((tweet) => tweet.id === id);
+        },
+        allUsers() {
+            return users;
+        },
+    },
+    Mutation: {
+        postTweet(root, { text, userId }) {
+            const newTweet = {
+                id: tweets.length + 1,
+                text,
+            };
+            tweets.push(newTweet);
+            return newTweet;
+        },
+        deleteTweet(root, { id }) {
+            if (tweets.find((tweet) => tweet.id === id)) {
+                const targetIndex = tweets.findIndex((tweet) => tweet.id === id);
+                tweets.splice(targetIndex, 1);
+                return true;
+            } else {
+                return false;
+            }
+        },
+    },
+    User: {
+        fullName({ firstName, lastName }) {
+            console.log(firstName, lastName);
+            return `${firstName}${lastName}`;
+        },
+    },
+};
+
 const typeDefs = gql`
     type User {
-        id: ID
-        userName: String
+        id: ID!
+        fullName: String
+        firstName: String
+        lastName: String
     }
     type Tweet {
-        id: ID
-        text: String
+        id: ID!
+        text: String!
         author: User
     }
     type Query {
-        allTweets: [Tweet]
-        tweet(id: ID): Tweet
+        allUsers: [User!]!
+        allTweets: [Tweet!]!
+        tweet(id: ID!): Tweet
+        ping: String!
+        pong: String!
+    }
+    type Mutation {
+        postTweet(text: String!, userId: ID!): Tweet!
+        deleteTweet(id: ID!): Boolean!
     }
 `;
 
-const server = new ApolloServer({ typeDefs });
+const server = new ApolloServer({ typeDefs, resolvers });
 
 server.listen().then(({ url }) => {
     console.log(`Running on ${url}`);
